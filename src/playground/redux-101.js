8586c7cfa47c9f6d04c983d4a5b8581a5d054a0c
Egidio <rafael.egidio@br.unisys.com>
2017-12-20 13:29:10 -0200

    changing redux state value

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,6 +1,25 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+
+    }
+
+    console.log('running');
     return state;
 
 });
@@ -12,7 +31,37 @@ console.log(store.getState());
 
 // Increment, decrement, reset
 
+
+
 // I`d like to increment the count
+store.dispatch({
+    type: 'INCREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+console.log(store.getState());
+
+
+store.dispatch({
+    type: 'RESET'
+});
+console.log(store.getState());
+
+
+
 // I`d like to reset count to zero.
 
-console.log(store.getState());
+
