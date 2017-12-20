e55d620501880624273e9706142b866d69751908
Egidio <rafael.egidio@br.unisys.com>
2017-12-20 13:49:26 -0200

    dynamic listener + default value + action on redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,14 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
@@ -25,7 +27,11 @@ const store = createStore((state = { count: 0 }, action) => {
 });
 
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
 
 //Actions - object thate get sent to the store
 
@@ -35,30 +41,27 @@ console.log(store.getState());
 
 // I`d like to increment the count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
-console.log(store.getState());
+
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy : 10
+});
 
 store.dispatch({
     type: 'DECREMENT'
-});
-console.log(store.getState());
 
-store.dispatch({
-    type: 'DECREMENT'
 });
-console.log(store.getState());
 
-store.dispatch({
-    type: 'DECREMENT'
-});
-console.log(store.getState());
 
 
 store.dispatch({
     type: 'RESET'
 });
-console.log(store.getState());
+
 
 
 
