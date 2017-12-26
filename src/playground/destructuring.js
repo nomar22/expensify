e3703a8c15d61a7998b10f8dd1fcd96ba4a0f61e
Egidio <rafael.egidio@br.unisys.com>
2017-12-26 16:06:23 -0200

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -48,4 +48,11 @@ const address = ['1299 S Juniper Street', 'Philadelphia', 'Pensylvania','19147']
 
 const [, city, state,zip, complement='Sem complemento'] = address;
 
-console.log (`You are in ${city} state of ${state}  and these are the complements ${complement}`)
+console.log (`You are in ${city} state of ${state}  and these are the complements ${complement}`);
+
+
+const item = ['Coffe (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, , mediumPrice] = item;
+console.log(`The medium ${itemName} costs ${mediumPrice}`);
+
