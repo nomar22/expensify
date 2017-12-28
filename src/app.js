3207e84a09ae2bfb227a6401d1d9d9e3d95dce96
Egidio <rafael.egidio@br.unisys.com>
2017-12-28 13:38:49 -0200

    filter using redux on real aplication

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses'
+import {setTextFilter} from './actions/filters'
+import getVisibleExpenses from './selectors/expenses.js'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
+
+//addExpense -> water bill
+//addExpense -> gas bill
+//setText Filter-> bill(2 items)
+//getVisibleExpeses ->print visibles ones to scree
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+
+});
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: 2 }));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
