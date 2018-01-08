cdc3db29e3bf3521bc4f3678e0552481d9c5f972
Egidio <rafael.egidio@br.unisys.com>
2018-01-08 12:57:26 -0200

    remove Button working

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {removeExpense} from '../actions/expenses';
+import {connect} from 'react-redux';
 
 // description, amount, createdAt
-export default ({description, createdAt, amount})=>(
+const ExpenseListItem = ({dispatch, id,description, createdAt, amount})=>(
     <div>
         <h3>{description}</h3>
+        <p>ID- {id}</p>
         <p>US$ {amount}, created at {createdAt}</p>
-        <button>Remove</button>
+        <button onClick={()=>{
+            dispatch(removeExpense({id}));
+        }}  >Remove</button>
     </div>
 );
+
+export default connect()(ExpenseListItem);
 
