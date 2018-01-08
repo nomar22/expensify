39c029bbdee762a97b967c3dd9ad2cd17806166a
Egidio <rafael.egidio@br.unisys.com>
2018-01-08 13:18:33 -0200

    filters apllieds

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -9,7 +9,7 @@ const ExpenseListFilters = (props) => (
         }} />
 
         <select  onChange={(e)=>{
-            e.target.value==='date'?props.dispatch(sortByDate()):props.dispatch(sortByAmount())
+            e.target.value==='date'?props.dispatch(sortByDate()):props.dispatch(sortByAmount());
         }}>
             <option value="date">Date</option>
             <option value="amount">Amount</option>
