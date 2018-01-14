25ab02aefae8d27e08ef9967919d1e8ca99352bb
Egidio <nomar22@gmail.com>
2018-01-14 13:10:25 -0200

    merge

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 // description, amount, createdAt
-export default ({description, createdAt, amount})=>(
+const ExpenseListItem =  ({description, createdAt, amount})=>(
     <div>
         <h3>{description}</h3>
         <p>US$ {amount}, created at {createdAt}</p>
         <button>Remove</button>
     </div>
 );
+
+
+export default connect ()(ExpenseListItem);
+
 
