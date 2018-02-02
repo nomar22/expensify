0a3630ca05ed13e31305a1bb6c697f82b24eb3a1
Egidio <nomar22@gmail.com>
2018-02-02 18:32:14 -0200

    filter using date Range

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -36,6 +36,9 @@ class ExpenseListFilters extends React.Component {
                     onDatesChange={this.onDatesChange}
                     focusedInput = {this.state.calendarFocused}
                     onFocusChange = {this.onFocusChange}
+                    numberOfMonths = {1}
+                    showClearDates= {true}
+                    isOutsideRange={()=>false}
 
                 />
             </div>
