927ea9b50b36ff46610e15f364f8dc6e636c5821
Egidio <nomar22@gmail.com>
2018-02-04 15:45:50 -0200

    filters tested

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,59 @@
+import {setStartDate, setEndDate,sortByAmount, sortByDate,setTextFilter} from '../../actions/filters';
+import moment from 'moment';
+
+
+test('Should generate set Start date action object', ()=>{
+    const action = setStartDate(moment(0));
+
+    expect(action).toEqual({
+        type:'SET_START_DATE',
+        startDate:moment(0)
+    });
+
+});
+
+
+
+test('Should generate set End date action object', ()=>{
+    const action = setEndDate(moment(0));
+
+    expect(action).toEqual({
+        type:'SET_END_DATE',
+        endDate:moment(0)
+    });
+});
+
+test('Should set generate sortByDate action object',()=>{
+    const action = sortByDate();
+
+    expect(action).toEqual({
+        type:'SORT_BY_DATE'
+    });
+});
+
+
+test('Should set generate sortByAmount action object',()=>{
+    const action = sortByAmount();
+
+    expect(action).toEqual({
+        type:'SORT_BY_AMOUNT'
+    });
+});
+
+test('Should set generate setTextFilter action object',()=>{
+    const action = setTextFilter('BILL');
+
+    expect(action).toEqual({
+        type:'SET_TEXT_FILTER',
+        text:'BILL'
+    });
+});
+
+
+test('Should set generate setTextFilter action object with default value',()=>{
+    const action = setTextFilter();
+    expect(action).toEqual({
+        type:'SET_TEXT_FILTER',
+        text:''
+    });
+});
