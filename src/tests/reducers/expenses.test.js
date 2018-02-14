e50c50db70b5918fa74dd9c185a651c814c9b32f
Egidio <nomar22@gmail.com>
2018-02-14 09:54:56 -0200

    test reducer finally

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -29,3 +29,55 @@ test ('Should not remove if id not found',()=>{
 
     expect(state).toEqual([expenses[0],expenses[1],expenses[2],expenses[3]]);
 });
+
+// should add an expense
+
+test('Should add an expense', ()=>{
+    const expense ={
+        description: 'Beer',
+        note: 'Beer at bar',
+        amount: 50,
+        createdAt: 0
+    }
+    const action = {
+        type:'ADD_EXPENSE',
+        expense
+    }
+
+    const state = expensesReducer(expenses,action );
+
+    expect(state).toEqual([...expenses,expense]);
+
+
+});
+
+// should edit an expense
+
+test ('Should edit an expense',()=>{
+    const action = {
+        type:'EDIT_EXPENSE',
+        id:expenses[1].id,
+        updates :{description:'Black Car'}
+    }
+
+    const state = expensesReducer(expenses,action);
+
+    expect(state[1].description).toEqual('Black Car');
+    
+
+});
+
+//should not edit if expense not found
+
+test ('Should edit an expense',()=>{
+    const action = {
+        type:'EDIT_EXPENSE',
+        id:'bla',
+        updates :{description:'Black Car'}
+    }
+
+    const state = expensesReducer(expenses,action);
+
+    expect(state).toEqual(expenses);
+
+});
