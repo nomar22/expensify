b03f6ad1f36418fa3a49d0f551778128f213a4f4
Rafael <nomar22@gmail.com>
2018-03-13 12:04:39 -0300

    new snapshots

---
+++ src/tests/component/ExpenseDashBoardPage.test.js
@@ -0,0 +1,12 @@
+import React from   'react';
+import {shallow} from 'enzyme';
+import {ExpenseDashBoardPage} from '../../components/ExpenseDashBoardPage';
+
+
+
+test('Should render expenseDashBoard Page ',()=>{
+    // const wrapper = shallow(<ExpenseDashBoardPage />);
+    // expect(wrapper).toMatchSnapshot();
+});
+
+
