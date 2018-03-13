9d43277797bc5fc82798049dab074db57338c970
Rafael <nomar22@gmail.com>
2018-03-12 23:31:22 -0300

    using enzyme-to-json

--- src/tests/component/Header.test.js
+++ src/tests/component/Header.test.js
@@ -6,11 +6,7 @@ import Header from '../../components/Header';
 
 test('Should render Header correctly', ()=>{
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify');
-    // const renderer = new ReactShallowRender();
-    // renderer.render(<Header/>);
-
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
 });
 
 
