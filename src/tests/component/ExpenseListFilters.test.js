968cca756cee44e8f1d58dd6fce3fdb479b4a47b
Rafael <nomar22@gmail.com>
2018-04-04 16:09:38 -0300

    test sort by amount and sort by date

--- src/tests/component/ExpenseListFilters.test.js
+++ src/tests/component/ExpenseListFilters.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import expenses from '../fixtures/expenses';
 import { filters, altFilters } from '../fixtures/filters';
+import { wrap } from 'module';
 
 
 
@@ -37,19 +38,40 @@ test('Should render ExpenseListFilter with alt data correctly', () => {
 
 
 test('Should handle text change',()=>{
+    const value = altFilters.date;
+    wrapper.find('input').at(0).simulate('change',{
+        target: {value}
+    });
+    expect(setTextFilters).toHaveBeenLastCalledWith(value);
 
 });
 
 
 test('Should sort by date',()=>{
+    const sortBy = filters.sortBy;
+    wrapper.find('select').simulate('change', {
+        target:{value: sortBy}
+    });
+
+    expect(sortByDate).toHaveBeenCalled();
+
 
 });
 
 test('Should sort by amount',()=>{
+   const sortBy = altFilters.sortBy;
+    wrapper.find('select').simulate('change', {
+        target:{value: sortBy}
+    });
 
+    expect(sortByAmount).toHaveBeenCalled();
+    
 });
 
-test('Should handle date     change',()=>{
+test('Should handle date  change',()=>{
+    const date = filters.date;
+     console.log(wrapper.find('withStyles(DateRangePicker)'))//.prop('onDateChange')(date,undefined);
+    // expect(setStartDate).toHaveBeenLastCalledWith(date);
 
 });
 
