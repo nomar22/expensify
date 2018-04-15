e18d28d50427fa338fb873658728d79645b9a590
Rafael <nomar22@gmail.com>
2018-04-15 19:11:13 -0300

    import initialize is needed whenever you want to use dates

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import {Link } from 'react-router-dom';
 
-// description, amount, createdAt
+// description, amount, createdAt 
 export const ExpenseListItem = ({id,description, createdAt, amount})=>(
     <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
