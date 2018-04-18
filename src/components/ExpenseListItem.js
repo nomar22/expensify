54e6b9c2f2ae60be9bc7ca7644070e0f4581520a
Rafael <nomar22@gmail.com>
2018-04-18 18:16:41 -0300

    formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,8 +8,8 @@ export const ExpenseListItem = ({id,description, createdAt, amount})=>(
     <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
         <p>
-            US$ {numeral(amount /100).format('$0,0.00') }
-            
+            US$ {numeral(amount /100).format('$0,0.00') } 
+            - 
             created at { moment(createdAt).format('MMMM Do, YYYY')}
         </p>
     </div>
