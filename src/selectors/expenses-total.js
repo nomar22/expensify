64684b1c1ee9f42e4cd4bb0ce74255ad0d2fc718
Rafael <nomar22@gmail.com>
2018-04-24 00:30:02 -0300

    refactoring

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,10 +1,8 @@
 
 //GetVisible Expenses
 export default (expenses) => {
-    if(expenses === undefined  || expenses.length == 0){
-        return 0;
-    }
     return expenses
     .map((expense) => expense.amount)
-    .reduce((total, actual)=> total + actual);
+    .reduce((total, actual)=> total + actual, 0);
+   
 };
