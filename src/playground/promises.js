9dafadd61a3602a25f55f2cea8ee1492d6dc1353
Rafael <nomar22@gmail.com>
2018-04-25 18:41:52 -0300

    learning about jsx promisses

---
+++ src/playground/promises.js
@@ -0,0 +1,20 @@
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+    //     resolve('This is my resolved data');
+    reject('Something went terrible wrong!');
+    }
+    ,5000)
+    
+});
+
+console.log('before');
+
+promise.then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log('error', error);
+});
+
+
+
+console.log('after');
