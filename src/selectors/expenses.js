27115efcc1148b9b2880444b8c81984f66db074f
Rafael <nomar22@gmail.com>
2018-05-16 22:48:55 -0300

    stable version

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -9,10 +9,12 @@ export default (expenses, { text, sortBy, startDate, endDate }) => {
 
         const startDateMatch = startDate? startDate.isSameOrBefore(createdAtMoment,'day'): true; 
         const endDateMatch =  endDate? endDate.isSameOrAfter(createdAtMoment, 'day'):true; 
-        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+        const textMatch =expense.description.toLowerCase().includes(text.toLowerCase());
+    
 
 
         return startDateMatch && endDateMatch && textMatch;
+        //  ;
     }).sort((a, b) => {
         if (sortBy === 'date') {
             return a.createdAt < b.createdAt ? 1 : -1;
