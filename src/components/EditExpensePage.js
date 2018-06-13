aa4164a2266ed4c8733c8e4c63a86ba5ae765466
Rafael <nomar22@gmail.com>
2018-06-12 22:05:42 -0300

    modal working perfectly #1

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -32,6 +32,19 @@ export class EditExpensePage extends React.Component {
                     </div>
 
                 </div>
+                <Modal
+                    isOpen={!!props.selectedOption}
+                    onRequestClose={props.handleCloseModal}
+                    contentLabel='Selected Option'
+                    closeTimeoutMS={200}
+                    className="modal"
+                >
+                    <h3 className="modal__title"> Selected Option </h3>
+                    {props.selectedOption && <p className="modal__body">{props.selectedOption} </p>}
+
+                    <button className="button" onClick={props.handleCloseModal}> OK </button>
+
+                </Modal>
             </div>
         );
     };
