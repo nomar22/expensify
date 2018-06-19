7fdf57e0fbfba8d78ee45d7f151cd25435de66b4
Rafael <nomar22@gmail.com>
2018-06-19 17:36:51 -0300

    sumary month with stateless component #19

--- src/components/ExpensesSumary.js
+++ src/components/ExpensesSumary.js
@@ -3,46 +3,36 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import totalExpenses from '../selectors/expenses-total';
 import numeral from 'numeral';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export class ExpensesSumary extends React.Component{
+export const ExpensesSumary = ({expenseCount, expenseTotal, leftExpenses}) => {
+    const expenseWord =expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedTotal = numeral(expenseTotal / 100).format('$0,0.00');
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                {
+                    (<div>
+                        <h1 className="page-header__title"> Viewing <span>{expenseCount}</span> of {leftExpenses} {expenseWord} totaling <span>{formattedTotal} </span></h1>
+                        <div className="page-header__actions">
+                            <Link className="button-layout" to="/create">Add Expense </Link>
+                        </div>
+                    </div>
+                    )
 
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //       monthExpenses:
-    //     };
-    // }
-
-    render() {
-        const expenseWord = this.props.expenseCount === 1 ? 'expense' : 'expenses';
-        const formattedTotal = numeral( this.props.expenseTotal / 100).format('$0,0.00');
-        return (
-            <div className="page-header">
-                <div className="content-container">
-                    {
-                            (<div>
-                                <h1 className="page-header__title"> Viewing <span>{ this.props.expenseCount}</span> of { this.props.leftExpenses} {expenseWord} totaling <span>{formattedTotal} </span></h1>
-                                <div className="page-header__actions">
-                                    <Link className="button-layout" to="/create">Add Expense </Link>
-                                </div>
-                            </div>
-                            )
-    
-    
-                    }
-                </div>
+                }
             </div>
-        );
-    };
+        </div>
+    );
 };
 
-const mapStateToProps = (state) => { 
+
+const mapStateToProps = (state) => {
     const expenses = selectExpenses(state.expenses, state.filters);
     const startDate = state.filters.startDate;
     const endDate = state.filters.endDate;
-    const generalExpenses = selectExpenses(state.expenses,{endDate,startDate});
+    const generalExpenses = selectExpenses(state.expenses, { endDate, startDate });
     return {
         expenseCount: expenses.length,
         expenseTotal: totalExpenses(expenses),
